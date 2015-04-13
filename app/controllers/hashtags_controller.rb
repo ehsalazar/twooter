cacc93c12b1132691d3e8716c5209ded08c1dc6b
Ernie Salazar <ehsalazar75@gmail.com>
2015-04-12 20:37:14 -0700

    Create hashtags controller with popular action.

---
+++ app/controllers/hashtags_controller.rb
@@ -0,0 +1,17 @@
+class HashtagsController < ApplicationController
+  def popular
+    hashtags = Hashtag.joins(:twoot_tags)
+              .select('hashtags.name, count(hashtag_id) as "hashtag_count"')
+              .group("hashtags.id")
+              .order('hashtag_count desc')
+              .limit(10)
+    hashtags.map! { |tag| tag.name }
+    render json: hashtags.to_json
+  end
+
+  private
+
+  def hashtag_params
+    params.require(:hashtag).permit(:name)
+  end
+end
